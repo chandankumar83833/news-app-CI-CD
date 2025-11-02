4d62a0b6a16e80af87e7acb1b6b74911a9e5ebe4
chandankumar83833 <chandanjustin888@gmail.com>
2025-11-02 17:47:47 +0530

    commit

---
+++ src/main/java/com/demo/app/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.demo.app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping("")
+	public String test() {
+		
+		return "App Testing chandan";
+		
+	}
+
+}
